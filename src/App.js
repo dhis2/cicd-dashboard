5e7ddb528eaaf1d0676845b86b642876777f88a0
Joakim Storløkken Melseth <joakim.melseth@gmail.com>
2022-10-26 16:37:21 +0200

    feat: example 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import { Octokit } from "@octokit/core";
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+
+const getWorkflowRun = async (repo) => {
+    const octokit = new Octokit({
+        auth: process.env.GITHUB_TOKEN,
+    });
+
+    const result = await octokit.request('GET /repos/dhis2/{repo}/actions/workflows/dhis2-verify-commits.yml/runs', {
+        branch: 'verify-commit-test',
+        repo,
+    });
+
+    const { status, conclusion, html_url } = result.data['workflow_runs'][0];
+    
+    return {
+        status,
+        conclusion,
+        html_url,
+    };
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HELLO <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [captureAppData, setCaptureAppData] = useState();
+    const [lineListingAppData, setlineListingAppData] = useState();
+
+    useEffect(() => {
+        getWorkflowRun('capture-app').then(res => {
+            setCaptureAppData(res);
+        });
+
+        getWorkflowRun('line-listing-app').then(res => {
+            setlineListingAppData(res);
+        });
+    }, []);
+
+    let  captureAppResult = "";
+    if (!captureAppData) {
+    } else if (captureAppData.status === 'completed') {
+        captureAppResult = captureAppData.conclusion; 
+    } else {
+        captureAppResult = captureAppData.status;
+    }
+
+    let lineListingAppResult = "";
+    if (!lineListingAppData) {
+    } else if (lineListingAppData.status === 'completed') {
+        lineListingAppResult = lineListingAppData.conclusion; 
+    } else {
+        lineListingAppResult = lineListingAppData.status;
+    }
+    debugger;   
+    return (
+        <>
+            <b>Verify commits workflow</b>
+            <p></p>
+            <div>
+                Capture app: {captureAppResult}
+                { captureAppData ? (<a href={captureAppData?.html_url}>link</a>) : null }
+            </div>
+            <div>
+                Line listing app: {lineListingAppResult}
+                { lineListingAppData ? (<a href={lineListingAppData?.html_url}>link</a>) : null }
+            </div>
+        </>
+    );
+
 }
 
 export default App;
